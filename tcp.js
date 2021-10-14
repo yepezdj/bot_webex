12fa4071c7ba51a68e2f6c04077cc5c1f4b52e87
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-13 21:46:25 -0500

    test

--- tcp.js
+++ tcp.js
@@ -1,7 +1,7 @@
 const net = require("net")
 
 const server = net.createServer(socket => {
-    socket.write("Hello.")
+    
     socket.on("data", data => {
         console.log(typeof data);
         console.log(data.toString())
