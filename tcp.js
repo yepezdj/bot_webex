7dda9a2801f9dfdb5ae4ba1c6df8bcefab8069f6
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-13 19:42:06 -0500

    test

--- tcp.js
+++ tcp.js
@@ -3,8 +3,12 @@ const net = require("net")
 const server = net.createServer(socket => {
     socket.write("Hello.")
     socket.on("data", data => {
+        console.log(typeof data);
         console.log(data.toString())
     })
 })
 
 server.listen(50000)
+
+
+
