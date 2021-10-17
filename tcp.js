d593532b56bf7986f594fa464e61bbfc1bd05114
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-17 16:49:46 -0500

    t

--- tcp.js
+++ tcp.js
@@ -1,26 +1,9 @@
-var http = require('http');
-var qs = require('querystring');
-var server = http.createServer(
+const express = require('express');
+const app = express();
+app.listen(50001, () => console.log('Listening at 50001'));
+app.use(express.static('public'));
 
-function (request, response) {
-    if (request.method == 'POST') {
-        var body = '';
 
-        request.on('data', function (data) {
-            body += data;
-
-            // Too much POST data, kill the connection!
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6)
-                request.connection.destroy();
-        });
-
-        request.on('end', function () {
-            var post = qs.parse(body);
-            console.log(post);
-        });
-    }
-});
-
-server.listen(50001);
-console.log("Server running on port 50001");
+app.post('/', (request,  response) => {
+    console.log(request.body);
+})
