bb82a4b1316ecd72529fcf8facfe15cfa88ff61b
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-17 14:05:45 -0500

    test

--- tcp.js
+++ tcp.js
@@ -1,14 +1,42 @@
 const net = require("net")
+var temp
 
 const server = net.createServer(socket => {
-    
+
     socket.on("data", data => {
         console.log(typeof data);
         console.log(data.toString())
+        proccess(data);
     })
 })
 
+function proccess(data) {
+
+    /* if ((data.resource) && (process.env.PORT)) {
+        specs.port = parseInt(process.env.PORT);
+        specs.access_token = process.env.TOKEN;
+        listener.verifyAccessToken(process.env.TOKEN).then((person) => {
+            console.log(fonts.info(`token authenticated as ${person.displayName}`));
+            specs.selection.event = 'created';
+            for (let resource_object of cli.firehose_resource_names) { 
+                listener.runListener(specs, cli.options[resource_object]);
+            }
+            }).catch(reason => {
+                //token not authorized
+                console.log(fonts.error(reason));
+                process.exit(-1);
+          });
+    } else {
+        cli.welcome();
+        gatherSpecs();
+    } */
+
+    console.log(Object.getOwnPropertyNames(data));
+
+
+
+}
+
+
+
 server.listen(50000)
-
-
-
