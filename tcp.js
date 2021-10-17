2a738b3f19136f1cdaf36a899b083c95c948958d
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-17 17:12:38 -0500

    t

--- tcp.js
+++ tcp.js
@@ -5,6 +5,6 @@ app.use(express.static('public'));
 app.use(express.json({ limit: '5mb' }));
 
 app.post('/', (request,  response) => {
-    console.log(request.body);
+    console.log(request.body.data);
     response.end();
 })
