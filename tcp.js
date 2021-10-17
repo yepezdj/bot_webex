3f5a0c5d9da25b1b4fab38a1144ef00c24b03740
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-17 16:35:29 -0500

    i

--- tcp.js
+++ tcp.js
@@ -1,20 +1,26 @@
 var http = require('http');
-var server = http.createServer ( function(request,response){
+var qs = require('querystring');
+var server = http.createServer(
 
-response.writeHead(200,{"Content-Type":"text\plain"});
-if(request.method == "GET")
-    {
-        response.end("received GET request.")
-    }
-else if(request.method == "POST")
-    {
-        response.end("received POST request.");
-    }
-else
-    {
-        response.end("Undefined request.");
+function (request, response) {
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+
+        request.on('end', function () {
+            var post = qs.parse(body);
+            console.log(post);
+        });
     }
 });
 
 server.listen(50001);
-console.log("Server running on port 8000");
+console.log("Server running on port 50001");
