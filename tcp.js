3f0bec2a48414c2d2848e544677a498efd45c44d
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-17 16:52:13 -0500

    r

--- tcp.js
+++ tcp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.listen(50001, () => console.log('Listening at 50001'));
 app.use(express.static('public'));
-
+app.use(express.json({ limit: '5mb' }));
 
 app.post('/', (request,  response) => {
     console.log(request.body);
