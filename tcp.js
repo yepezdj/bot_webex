18a18cfa0bdeb7d91ec78ad46d9ce36fbf9ce612
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-17 16:13:07 -0500

    t

--- tcp.js
+++ tcp.js
@@ -1,43 +1,20 @@
-const net = require("net")
-var temp
+var http = require('http');
+var server = http.createServer ( function(request,response){
 
-const server = net.createServer(socket => {
+response.writeHead(200,{"Content-Type":"text\plain"});
+if(request.method == "GET")
+    {
+        response.end("received GET request.")
+    }
+else if(request.method == "POST")
+    {
+        response.end("received POST request.");
+    }
+else
+    {
+        response.end("Undefined request.");
+    }
+});
 
-    socket.on("data", data => {
-        //console.log(typeof data);
-        console.log(data.toString())
-        //proccess(data);
-    })
-})
-
-function proccess(data) {
-
-    /* if ((data.resource) && (process.env.PORT)) {
-        specs.port = parseInt(process.env.PORT);
-        specs.access_token = process.env.TOKEN;
-        listener.verifyAccessToken(process.env.TOKEN).then((person) => {
-            console.log(fonts.info(`token authenticated as ${person.displayName}`));
-            specs.selection.event = 'created';
-            for (let resource_object of cli.firehose_resource_names) { 
-                listener.runListener(specs, cli.options[resource_object]);
-            }
-            }).catch(reason => {
-                //token not authorized
-                console.log(fonts.error(reason));
-                process.exit(-1);
-          });
-    } else {
-        cli.welcome();
-        gatherSpecs();
-    } */
-    //var jsonArray = '{"required":1, "minlength":2}'
-    var obj = JSON.parse(data);
-    console.log(typeof obj);
-    console.log(Object.values(obj));
-
-
-}
-
-
-
-server.listen(50001)
+server.listen(50001);
+console.log("Server running on port 8000");
