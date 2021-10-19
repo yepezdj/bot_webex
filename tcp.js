f541eeb8890e7d876da3b478c413258cced4e970
Jesus Yepez <yepezdj2014@gmail.com>
2021-10-18 20:05:39 -0500

    t

--- tcp.js
+++ tcp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
 app.listen(50001, () => console.log('Listening at 50001!'));
@@ -28,7 +28,86 @@ function welcome(data) {
     if (data.text) {
         console.log(data.text)
         //var myHeaders = new Headers();
-        var myHeaders = new fetch.Headers();
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://webexapis.com/v1/messages", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', process.env.TOKEN_BEARER);
+        xhr.send(JSON.stringify({
+            "toPersonEmail": data.personEmail,
+            "text": "test from postman",
+            "attachments": [{
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "content": {
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "type": "AdaptiveCard",
+                    "version": "1.2",
+                    "body": [{
+                            "type": "TextBlock",
+                            "text": "Bot assistant Security team",
+                            "size": "Large",
+                            "weight": "Bolder"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": "Hi! I am SEC-BOT, I am a bot that can help you solve some",
+                            "isSubtle": true
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": "inquires about working in Sykes, the cisco security team and",
+                            "isSubtle": true,
+                            "spacing": "None"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": "also help you with you cases give you some basic ",
+                            "isSubtle": true,
+                            "spacing": "None"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": " troubleshooting steps.",
+                            "isSubtle": true,
+                            "spacing": "None"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": "Tell me. How can I help you today?"
+                        }
+                    ],
+                    "actions": [{
+                            "type": "Action.Submit",
+                            "title": "Sykes",
+                            "data": {
+                                "action": "sykes"
+                            }
+                        },
+                        {
+                            "type": "Action.Submit",
+                            "title": "Security team",
+                            "data": {
+                                "action": "sec"
+                            }
+                        },
+                        {
+                            "type": "Action.Submit",
+                            "title": "Troubleshoting",
+                            "data": {
+                                "action": "ts"
+                            }
+                        }
+                    ]
+                }
+            }]
+        }));
+        xhr.onload = function () {
+            console.log("HELLO")
+            console.log(this.responseText);
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+        }
+        /* var myHeaders = new fetch.Headers();
         myHeaders.append("Authorization", process.env.TOKEN_BEARER);
         myHeaders.append("Content-Type", "application/json");
 
@@ -112,7 +191,8 @@ function welcome(data) {
             .then(response => response.text())
             .then(result => console.log(result))
             .catch(error => console.log('error', error));
-    }
+            */
+    } 
 }
 
 function cards() {
