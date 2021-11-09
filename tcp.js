fcebb86efd9136f4fccf4350d511a29e29f65fdd
Jesus Yepez <yepezdj2014@gmail.com>
2021-11-08 23:17:47 -0500

    o

--- tcp.js
+++ tcp.js
@@ -53,15 +53,13 @@ function verify(data) {
             var now = Date.now();
             var dif = now - result[0].timestamp;
             //Verify last interaction
-            if (dif >= 300000) {
+            if (dif >= 300) {
                 let sql2 = `UPDATE users SET timestamp ='${now}' WHERE email ='${email}'`;
                 database.query(sql2, function (err, result) {
                     if (err) throw err;
                 });
                 welcome(data);
             }
-
-
         } else {
             console.log('no pistola')
             database.connect(function (err) {
